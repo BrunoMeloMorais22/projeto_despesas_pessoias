fd088fa6fe0fb945a5939470c3d5eaba1bf8bf02
Bruno <bruno9405@fito.br>
2026-06-07 10:24:01 -0300

    ds

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -6,7 +6,8 @@ document.getElementById('expenseForm').addEventListener("submit", async(e) => {
         descricao: document.getElementById('descricao').value,
         valor: Number(document.getElementById('valor').value),
         categoria: document.getElementById('categoria').value,
-        data: document.getElementById('data').value
+        data: document.getElementById('data').value,
+        data2: document.getElementById('sdad').value
     }
 
     const token = localStorage.getItem('token')
