f9d5a4e31b6d87424c8715237924806e31842cfe
Bruno <bruno9405@fito.br>
2026-06-08 15:08:37 -0300

    Criando o gráfico

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,24 +1,46 @@
-async function carregarDespesas() {
+async function carregarDashboard() {
+
     const response = await fetch(
         'http://localhost:3000/auth/expenses'
     )
 
     const despesas = await response.json()
 
+    // TABELA
     const tabela = document.getElementById('tabelaExpenses')
 
     tabela.innerHTML = ''
 
-    despesas.forEach(despesas => {
+    despesas.forEach(despesa => {
         tabela.innerHTML += `
             <tr>
-                <td>${despesas.descricao}</td>
-                <td>${despesas.valor}</td>
-                <td>${despesas.categoria}</td>
-                <td>${despesas.data}</td>
+                <td>${despesa.descricao}</td>
+                <td>${despesa.valor}</td>
+                <td>${despesa.categoria}</td>
+                <td>${despesa.data}</td>
             </tr>
         `
     })
+
+    // GRÁFICO
+    const categorias = {}
+
+    despesas.forEach(despesa => {
+        categorias[despesa.categoria] =
+            (categorias[despesa.categoria] || 0) + Number(despesa.valor)
+    })
+
+    const ctx = document.getElementById('expenseChart')
+
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: Object.keys(categorias),
+            datasets: [{
+                data: Object.values(categorias)
+            }]
+        }
+    })
 }
 
-carregarDespesas()
+carregarDashboard()
