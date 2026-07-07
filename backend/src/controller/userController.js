fb93536e3c25f41379cb3a82bdfced13fd006691
Bruno <bruno9405@fito.br>
2026-07-07 11:10:08 -0300

    Adicionando regras de senha

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const userService = require('../service/userService')
+const { registerSchema } = require('../validators/userValidator')
+const { loginSchema } = require('../validators/userValidator')
 
 exports.register = async(req, res) => {
     console.log("Entrou no register");
     try{
+
+        registerSchema.parse(req.body)
+
         const { nome, email, senha } = req.body
 
         const result = await userService.register(
@@ -25,6 +30,9 @@ exports.register = async(req, res) => {
 
 exports.login = async(req, res) => {
     try{
+
+        loginSchema.parse(req.body)
+
         const { email, senha } = req.body
 
         const result = await userService.login(
